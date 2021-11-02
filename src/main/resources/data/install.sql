87c5f68cc3de0270ae2cf6cae24dbc1a61e98fd4
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-02 14:03:17 +0100

    initial install sql script

---
+++ src/main/resources/data/install.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS user(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    birthdate DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS wishlist (
+    wishlist_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS wish(
+    wish_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    price VARCHAR(255) NOT NULL,
+    wishlist_id INT NOT NULL,
+    FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
+);
