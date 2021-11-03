c066d22a0094787ba0dac92f3b0f5693b2b20dd1
Mathias Reker <mathias@reker.dk>
2021-11-03 11:02:05 +0100

    add schema

--- src/main/resources/data/install.sql
+++ src/main/resources/data/install.sql
@@ -1,8 +1,11 @@
+create schema IF NOT EXISTS wishshare;
+use wishshare;
+
 DROP TABLE IF EXISTS wish;
 DROP TABLE IF EXISTS wishlist;
 DROP TABLE IF EXISTS user;
 
-CREATE TABLE IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS user (
     user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -15,16 +18,17 @@ CREATE TABLE IF NOT EXISTS wishlist (
     wishlist_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (user_id)
+    REFERENCES user (user_id)
 );
 
-
-CREATE TABLE IF NOT EXISTS wish(
+CREATE TABLE IF NOT EXISTS wish (
     wish_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     link VARCHAR(255) NOT NULL,
     price VARCHAR(255) NOT NULL,
     wishlist_id INT NOT NULL,
     reserved TINYINT NOT NULL,
-    FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
+    FOREIGN KEY (wishlist_id)
+    REFERENCES wishlist (wishlist_id)
 );
