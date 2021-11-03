61adc7dda7eb77693e43ec471c68fc72fe3c0dd6
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-03 11:43:34 +0100

    Added new methods for retrieving data and adding data.
    changed builder to static.
    
    Added reset to builder in wishlist.

--- src/main/java/com/group6/wishshare/data/repository/WishListRepository.java
+++ src/main/java/com/group6/wishshare/data/repository/WishListRepository.java
@@ -1,31 +1,76 @@
 package com.group6.wishshare.data.repository;
 
 import com.group6.wishshare.data.Util.DbManager;
+import com.group6.wishshare.domain.model.Wishlist;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class WishListRepository {
 
-    Connection connection = DbManager.getInstance().getConnection();
+  Connection connection = DbManager.getInstance().getConnection();
 
-    public boolean addWishList(String name, int userid){
-        String stm = "INSERT INTO wishlist (name, user_id) VALUES (? , ?)";
-        PreparedStatement prep;
 
-        try{
-            prep = connection.prepareStatement(stm);
-            prep.setString(1,name);
-            prep.setInt(2, userid);
 
-            boolean result = prep.execute();
 
-            return  result;
+  /**
+   * Method to add a wishlist to database.
+   *
+   * @Returns true if successful, false if not.
+   *
+   * @auther Andreas
+   */
 
-        } catch (SQLException throwables) {
-            return false;
-        }
+  public boolean addWishList(String name, int userid) {
+    String stm = "INSERT INTO wishlist (name, user_id) VALUES (? , ?)";
+    PreparedStatement prep;
 
+    try {
+      prep = connection.prepareStatement(stm);
+      prep.setString(1, name);
+      prep.setInt(2, userid);
+
+      boolean result = prep.execute();
+
+      return result;
+
+    } catch (SQLException e) {
+      return false;
     }
+  }
+
+  /**
+   * Method to retrieve a wishlist object from database using wishlist index.
+   *
+   * @Returns wishlist object or null - i dont like this tho...
+   *
+   * @auther Andreas
+   */
+
+  public Wishlist getWishlist(int id) {
+    String stm = "SELECT * FROM wishlist WHERE wishlist_id = ?";
+    PreparedStatement prep;
+    Wishlist wishlist;
+
+    try {
+      prep = connection.prepareStatement(stm);
+      prep.setInt(1, id);
+      ResultSet resultSet = prep.executeQuery();
+      if (resultSet.next()) {
+        wishlist =
+            new Wishlist.WishListBuilder()
+                .id(resultSet.getInt(1))
+                .name(resultSet.getString(2))
+                .userid(resultSet.getInt(3))
+                .build();
+        return wishlist;
+      }
+
+    } catch (SQLException e) {
+      //do something
+    }
+    return null;
+  }
 }
