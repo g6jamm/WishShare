2c6575bd9da9dcd88b4da83cb95b3b981beed47d
jacki <jack3459@stud.kea.dk>
2021-11-09 12:27:23 +0100

    moved isvaliduser to userService

--- src/main/java/com/group6/wishshare/domain/service/UserService.java
+++ src/main/java/com/group6/wishshare/domain/service/UserService.java
@@ -1,12 +1,22 @@
 package com.group6.wishshare.domain.service;
 
+import com.group6.wishshare.data.repository.DataFacade;
 import com.group6.wishshare.data.repository.UserRepository;
 import com.group6.wishshare.domain.model.User;
+import org.springframework.web.context.request.WebRequest;
 
 public class UserService {
-  private UserRepository userRepository = new UserRepository();
+  private final UserRepository userRepository = new UserRepository();
+  private final LoginService loginService = new LoginService(new DataFacade());
 
   public User getUser(int userId) {
     return userRepository.getUser(userId);
+  }
+
+  public boolean isValidUser(Integer userId) {
+    if (userId != null) {
+      return loginService.userExist(userId);
+    }
+    return false;
   }
 }
